f74def60baa0e23c5cd467ed29217ac8122bf357
ducnq <>
2022-02-14 17:01:43 +0700

    update

--- Documents/Dev/NextJs/netflix-clone/next.config.js
+++ Documents/Dev/NextJs/netflix-clone/next.config.js
@@ -1,10 +1,7 @@
-const withSass = require('@zeit/next-sass');
-const withImages = require('next-images');
-const withLess = require('@zeit/next-less')
-const withCSS = require('@zeit/next-css')
+const path = require('path')
 
-module.exports = withCSS(withLess(withImages(withSass({
-    env: {
-        ANY_ENV_KEY: "ANY_ENV_VARIABLE"
-    }
-}))));
+module.exports = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+}
--- Documents/Dev/NextJs/netflix-clone/pages/index.tsx
+++ Documents/Dev/NextJs/netflix-clone/pages/index.tsx
@@ -2,6 +2,7 @@ import { Button, TextField } from "@material-ui/core";
 import Image from "next/image";
 import React from "react";
 import Logo from "../public/images/logo.svg";
+import App from "../public/images/netflix-app.jpeg";
 
 export default function Index() {
   return (
@@ -44,6 +45,24 @@ export default function Index() {
             </form>
           </div>
         </div>
+      </div>
+      <div className="line"></div>
+      <div className="app">
+        <div className="description">
+          <h1>Have an Android Phone? Get our new free plan!</h1>
+          <h3>
+            Watch a selection of new movies and TV shows without adding any
+            payment details!
+          </h3>
+          <a href="#">Get the app </a>
+        </div>
+        <div className="app-logo">
+          <Image src={App} />
+        </div>
+      </div>
+      <div className="line"></div>
+      <div className="bottom">
+        
       </div>
     </div>
   );
